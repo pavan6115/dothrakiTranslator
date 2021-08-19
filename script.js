2fe7c39d502f3c7929f08cf7fea4bd121f2d6792
kpavann <kulkarnipavan6115@gmail.com>
2021-08-19 11:59:20 +0530

    feat:logic done

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log('connected!')
+const translateBtn = document.querySelector('#translateBtn')
+const inpText = document.querySelector('.inpText')
+const outputSection = document.querySelector('.output')
+
+const serverURL = "https://api.funtranslations.com/translate/dothraki.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+
+function clickHandler() {
+    const inputText = inpText.value
+    // server processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        const translatedText = json.contents.translated;
+        outputSection.innerText = translatedText
+    })
+    .catch(errorHandling)
+}
+
+function errorHandling(err) {
+    console.log('Error occured - ', err)
+    alert('Server Issue, please try again after sometime. For error, check console')
+}
+
+
+translateBtn.addEventListener('click', clickHandler)
